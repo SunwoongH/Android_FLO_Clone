4f02f96d50bc0350fe01a88ff35fe91d1dcca485
SunWoong <sunwoong1122@gmail.com>
2022-04-02 01:34:00 +0900

    Feat: locker fragment TabLayout & ViewPager 구현

--- app/src/main/java/com/example/joy/LockerFragment.kt
+++ app/src/main/java/com/example/joy/LockerFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.joy.databinding.FragmentLockerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LockerFragment : Fragment() {
     lateinit var binding: FragmentLockerBinding
+    private val tapInformation = arrayListOf("저장한 곡", "음악 파일")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +18,13 @@ class LockerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+
+        var lockerAdapter = LockerVPAdapter(this)
+        binding.songLockerVp.adapter = lockerAdapter
+        TabLayoutMediator(binding.songLockerTb, binding.songLockerVp) {
+            tab, position -> tab.text = tapInformation[position]
+        }.attach()
 
         return binding.root
     }
